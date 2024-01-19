eb9fa8daad3d8e71050b6893200d73a0a4cdbc03
Rahul Boga <rahul.boga410@gmail.com>
2024-01-18 21:46:30 -0500

    Added home page

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+type Tab = {
+  name: string;
+  icon?: string;
+}
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  tabs: Tab[] = [
+    {
+      name: 'Home',
+      icon: 'fi-rr-home'
+    },
+    {
+      name: 'Groups',
+      icon: 'fi-rr-users-alt'
+    },
+    {
+      name: 'Friends',
+      icon: 'fi-rr-following'
+    },
+    {
+      name: 'Settings',
+      icon: 'fi-rr-settings'
+    },
+    {
+      name: 'Invite',
+      icon: 'fi-rr-invite'
+    },
+    {
+      name: 'Logout',
+      icon: 'fi-rr-sign-out-alt'
+    }
+]
 
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { BrowserModule, platformBrowser } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
   ],
   exports: [
     HeaderComponent
